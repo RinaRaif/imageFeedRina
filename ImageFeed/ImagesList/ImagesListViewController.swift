5c24afac05d6fd6799b8fe7626bbe625d7243902
Rina <cimas@ya.ru>
2024-05-21 16:40:01 +0300

    Зокончена логика приложения по 8 спринту. Наполнена лента изображений - всего их 20. Добавлена кнопка лайка с цветом на четные фото, отображен текс лейбла даты (как текущая), лента прокручивается, края фото скруглены

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
     
+    private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    
+    private lazy var dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
+        return formatter
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.rowHeight = 200
+        tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
     }
 }
 
 extension ImagesListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return photosName.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -27,11 +37,11 @@ extension ImagesListViewController: UITableViewDataSource {
         // 1 Извлекаем ячейку cell из пула переиспользуемых ячеек таблицы с идентификатором заданным в imagesListCell.
         
         guard let imageListCell = cell as? ImagesListCell else {
-            // 2 Выполняем проверку: можно ли привести ячейку к типу imagesListCell.
             return UITableViewCell()
+            // 2 Выполняем проверку: можно ли привести ячейку к типу imagesListCell.
         }
         
-        configCell(for: imageListCell)
+        configCell(for: imageListCell, with: indexPath)
         // 3 Метод который настраивает содержимое ячейки.
         return imageListCell
         // 4 Возвращаем ячейку типа ImagesListCell
@@ -39,9 +49,35 @@ extension ImagesListViewController: UITableViewDataSource {
 }
 
 extension ImagesListViewController {
-    func configCell(for cell: ImagesListCell) { }
+    func configCell(for cell: ImagesListCell, with indexPath: IndexPath) {
+        guard let image = UIImage(named: photosName[indexPath.row]) else {
+            return
+        } // 1 Извлекаем изображение по имени их массива по индексу через метод .row, иначе возвращаем nil.
+        
+        cell.cellImage.image = image // 2 Устанавливаем изображение для свойства image у ячейки cell.
+        cell.dateLabel.text = dateFormatter.string(from: Date()) // 3 Форматируем дату и устанавливаем как текст в DateLabel.
+        
+        if indexPath.row % 2 == 0 {
+            cell.likeButton.setImage(UIImage(named: "like_button_on"), for: .normal)
+        } else {
+            cell.likeButton.setImage(UIImage(named: "like_button_off"), for: .normal)
+        } // 4 Используем конструкцию if-else для установки изображения кнопки "лайк" в зависимости от четности индекса строки.
+    }
 }
+    extension ImagesListViewController: UITableViewDelegate {
+        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+        
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            guard let image = UIImage(named: photosName[indexPath.row]) else {
+                return 0
+            }
+            
+            let imageInsets = UIEdgeInsets(top: 4, left: 16, bottom: 4, right: 16)
+            let imageViewWidth = tableView.bounds.width - imageInsets.left - imageInsets.right
+            let imageWidth = image.size.width
+            let scale = imageViewWidth / imageWidth
+            let cellHeight = image.size.height * scale + imageInsets.top + imageInsets.bottom
+            return cellHeight
+        }
+    }
 
-extension ImagesListViewController: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {}
-}
