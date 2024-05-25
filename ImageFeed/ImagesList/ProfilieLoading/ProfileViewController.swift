971a5ed51f996e50642a90fa025575c7831cd0f5
Rina <cimas@ya.ru>
2024-05-25 08:44:05 +0300

    Сделана верстка экрана Пользователя, добавлены лейблы, кнопка, выставлены констрейнты

---
+++ ImageFeed/ImagesList/ProfilieLoading/ProfileViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Рина Райф on 23.05.2024.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
